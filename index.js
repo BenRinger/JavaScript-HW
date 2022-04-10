1d5538575a5791b39f939cbc8a63804ab2a0c936
Ben <ringerben1@gmail.com>
2022-04-09 20:33:24 -0400

    first push

---
+++ index.js
@@ -0,0 +1,34 @@
+alert("L");
+// This is a comment. you cant even see me.
+alert("F");
+/*
+You 
+still
+cant
+see
+me
+
+
+
+Example JavaScript below
+
+alert('hello');
+
+
+20 + 20;
+
+var myVar = 10;
+
+myVar + 10;
+
+myVar += 10;
+
+myVar /= 2
+
+myVar *= 4
+
+myVar -=3
+
+++myVar
+
+*/
